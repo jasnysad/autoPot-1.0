d89cbf46e549f2ffd122e8d891c6bb95e5c21e6c
jasnysad <sadjasny@gmail.com>
2025-08-03 05:43:20 +0200

    1st
    get off me perv

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import keyboard as k
+import pyautogui as py
+import time as t
+
+p = print
+
+wlacz = 'x'         # Aktywacja
+wyjscie = '='       # De-aktywacja
+delay = 0.01        # Delay przed zmiana
+
+def jasnysad(event):
+    if event.name == wlacz and event.event_type == 'down':
+        sw, sh = py.size()
+        s_x, s_y = sw // 2, sh // 2
+        
+        op = py.position()
+        
+        py.moveTo(s_x, sh)
+        t.sleep(delay)
+        
+        k.press('6')
+        py.rightClick()
+        k.release('6')
+        
+        t.sleep(0.01)
+        
+        k.press('9')
+        py.rightClick()
+        k.release('9')
+        
+        py.moveTo(s_x, s_y)
+        t.sleep(delay)
+
+k.hook(jasnysad)
+
+p(r"""
+.-. .-.  .--.  .---.  .--.  .-. .-.  .--.  
+| |/ /  / {} \{_   _}/ {} \ |  `| | / {} \ 
+| |\ \ /  /\  \ | | /  /\  \| |\  |/  /\  \
+`-' `-'`-'  `-' `-' `-'  `-'`-' `-'`-'  `-'   by @jasnysad for @4fth
+
+                        bind: X
+""")
+k.wait(wyjscie)
+#       ^ bind off
