a259d552ac85ec143abc8e5582488da7c5d208ac
JasnySąd <sadjasny@gmail.com>
2025-08-04 03:23:45 +0200

    Update main.py 1.2

--- src/main.py
+++ src/main.py
@@ -1,35 +1,42 @@
+from pynput.mouse import Controller, Button
 import keyboard as k
-import pyautogui as py
 import time as t
+
+mouse = Controller()
 
 p = print
 
-wlacz = 'x'         # Aktywacja            | bind x
-wyjscie = '='       # De-aktywacja         | bind =
-delay = 0.01        # Delay przed zmiana   | delay
+wlacz = 'x'         # Aktywacja                | bind x
+wyjscie = '='       # De-aktywacja             | bind =
+delay = 0.01        # Delay przed zmiana slotu 
+cooldown = 0.5      # Minimalny odstęp czasowy między kolejnymi aktywacjami funkcji ruch/klik
+ltt = 0             # Timestamp ostatniego wywołania funkcji, potrzebny do sprawdzenia cooldown
+
+
+def ruch(dy, steps=15, delay_per_step=0.005):
+    dy_per_step = dy / steps
+    for _ in range(steps):
+        mouse.move(0, dy_per_step)
+        t.sleep(delay_per_step)
 
 def jasnysad(event):
+    global ltt
     if event.name == wlacz and event.event_type == 'down':
-        sw, sh = py.size()
-        s_x, s_y = sw // 2, sh // 2
-        
-        op = py.position()
-        
-        py.moveTo(s_x, sh)
+        if t.time() - ltt < cooldown:
+            return
+        ltt = t.time()
+
+        ruch(900)
+
+        k.send('1')
+        mouse.click(Button.right)
+
         t.sleep(delay)
-        
-        k.press('6')
-        py.rightClick()
-        k.release('6')
-        
-        t.sleep(0.01)
-        
-        k.press('R')
-        py.rightClick()
-        k.release('R')
-        
-        py.moveTo(s_x, s_y)
-        t.sleep(delay)
+
+        k.send('2')
+        mouse.click(Button.right)
+
+        ruch(-900)
 
 k.hook(jasnysad)
 
@@ -42,4 +49,3 @@ p(r"""
                         bind: X
 """)
 k.wait(wyjscie)
-
